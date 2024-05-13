fd17efa5c0985534294e3dff3bd0da50833b6eb7
FireSoul04 <dedde.mancio@gmail.com>
2024-05-13 21:59:51 +0200

    terzo concluso

--- Architetture/Elaborato/Terzo/terzo.c
+++ Architetture/Elaborato/Terzo/terzo.c
@@ -28,45 +28,55 @@ void main()
         cmp ebx, 0                      // Controllo se la lunghezza è 0
         je fine
         mov ch, 8                       // Contatore per gestire quando passare al prossimo byte
-        lea esi, vet
-    estrai_byte:
-        sub ch, n
-        dec ch
-        cmp ch, 0
-        jg ottieni_byte
-        inc esi                         // Passo al prossimo byte
-        mov ch, 8                       // Il contatore torna a 8
-    ottieni_byte:
+        lea esi, vet                    // Salvo in ESI il puntatore del primo byte
         mov al, [esi]
+    ciclo:
+        cmp errori, 1                   // Al primo errore che trova finisce il programma
+        je fine
+        xor ah, ah                      // Azzero il bit di parità di un blocco
         mov cl, n
-        cmp al, 0
-        je estrai_byte
-    bit_parita:
-        //cmp errori, 1                   // Al primo errore che trova finisce il programma
-        //je fine
-        mov dl, al
-        and dl, 1                       // Salvo il bit di parità in DL
     estrai_bit:
-        cmp al, 0
-        je estrai_byte                  // Quando non sono presenti bit a uno passo al prossimo blocco di bit
+        test al, 1
+        jz pari                         // Se l'and restituisce 0 AH è pari e non aggiungo un bit alla parità
+        xor ah, 1                       // Conto la parità in AH, uso l'XOR per evitare di gestire la carry
+    pari:
+        shr al, 1
+    segui_byte:
         dec ebx
         cmp ebx, 0                      // Controllo quando ho finito di scorrere i bit
         je fine
-        mov ah, al
-        and ah, 1 
-        xor ah, 1                       // Conto la parità in AH, uso l'XOR per evitare di gestire la carry
-        shr al, 1
+        call controlla_byte
         dec cl
         cmp cl, 0
         jg estrai_bit
+    bit_parita:
+        mov dl, al
+        and dl, 1                       // Salvo il bit di parità in DL
+        shr al, 1                       // Passo al primo bit del blocco
         call controlla_parita
-        jmp fine
+        call controlla_byte
+        dec ebx
+        cmp ebx, 0                      // Controllo quando ho finito di scorrere i bit
+        je fine
+        jmp ciclo
 
+    // Confronto il bit di parità con la parità del blocco di bit
     controlla_parita:
         cmp ah, dl
-        je fine_controllo               // Se la parità dei bit e il bit di parità sono uguali inizializzo errori a 1
-        or errori, 1
+        je fine_controllo
+        or errori, 1                    // Se la parità dei bit e il bit di parità sono uguali inizializzo errori a 1
     fine_controllo:
+        ret
+
+    // Controlla se devo controllare il prossimo byte del vettore
+    controlla_byte:
+        dec ch                          // Contatore di bit, quando arriva a 0 passo al byte successivo
+        cmp ch, 0
+        jg fine_controllo_byte
+        inc esi                         // Passo al prossimo byte
+        mov ch, 8
+        mov al, [esi]                   // Prende il byte successivo
+    fine_controllo_byte:
         ret
 
     fine:
